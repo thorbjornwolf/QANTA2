1fb2135ff6cff52f53a37659cee3d88ff101676f
Thorbjørn Wolf <thorbjorn.wolf@gmail.com>
2015-12-03 22:21:19 +0900

    Comments for Z

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
+# Import unused. Remove it.
 from collections import defaultdict
+
+# See if you can find a different name than 'main.py'
+# It sounds so preposterous, especially when it is
+# only supposed to handle preprocessing.
 
 #Not working yet, probably many errors
 
@@ -8,10 +13,17 @@ def main(path):
 	questions_list=[]
 
  	# Parse the questions from the CSV file, outputs a list
+ 	# NOTE: Hardcoding local paths is a definite no-no. It's ok for early 
+ 	# development, though, but should ASAP be converted to use either the 
+ 	# local config or an input argument
+ 	# You will need to `import csv_handler` for this to work
  	questions = csv_handler.parse_question_csv("C:\Users\zaki\Desktop\QANTA2-master\questions20k.csv")
 
  	# For better manageability
  	for item in questions:
+ 		# questions_list is simply a list, it has no method `add_question`
+ 		# You could, as mentioned below, use a namedtuple to store question lines,
+ 		# and then `append` those to this list
  		questions_list.add_question(item[0], item[1], item[2], item[3] , item[4])
 	
  	# Parse the data using Stanford parser and then add it to the dependency tree
@@ -24,6 +36,8 @@ def main(path):
  		if load_status % 1000 == 0:
 			print load_status 
 
+		# This class simply wraps the DependencyTree constructor (__init__)
+		# Might as well remove it.
 		Create_tree.Init(parsed_tree, question_id, answer_index)
  	
  	#########################################
@@ -31,37 +45,58 @@ def main(path):
  	#########################################
 
 
-
+# This class doesn't seem to be needed
+# Just remove it
 class Create_tree(object):
 	Dependency_tree = None
 
 	def Init(tree):
 		Dependency_tree = Dependency_tree(tree, question_id, answer_index)
 
+# Python class naming style is DependencyTree
 class dependency_tree(object):
 
 	def __init__(self, tree, question_id, answer_index):
 
+		# Wait initializing the node until you have something ready to 
+		# put in it. This gives an error.
 		self.root = Dependency_tree_node()
 		self.answer_index = answer_index
 		self.question_id = question_id
+		# You already know the number of nodes: len(tree)
+		# Initialize this directly, instead of to 0
 		self.n_nodes = 0
 
+		# I am not sure about the order of the nodes. Better err on the safe
+		# side and construct the tree by a queue: If the node has a place in
+		# the tree, i.e. is another nodes child, insert it. Otherwise put it
+		# back in the queue. This is a bit inefficient, but it will get the 
+		# job done. If you can think of a better way, please go ahead!
 		for node in tree:
+			# You already know the number of nodes: len(tree)
+			# No need to += 1 for each iteration
 			self.n_nodes+=1
 			print node
+			# DependencyNode.Add_child is not implemented yet.
+			# Also, code style dictates that we use DependencyNode.add_child
+			# instead.
 			self.root.Add_child(surface, uris)
 
+# Python class naming style is DependencyNode (also, see dev notes)
 class dependency_tree_node(object):
 	
 	def __init__(self, word_index, dependency_index):
 
 		self.word_index = word_index
 		self.dependency_index = dependency_index
+		# children should be a list, not a set or dict.
 		self.children = {}
 
+# Use a namedtuple for this, instead of a full class. They can be found in 
+# collections
 class Data:
 
+	# You forgot a colon at the end here :)
 	def __init__(self, question_id, fold, answer, category, text)
 
 		self.question_id = question_id
@@ -69,3 +104,11 @@ class Data:
 		self.answer = answer
 		self.category = category
 		self.text = text
+
+# Overall suggestion:
+# Use divide and conquer: Develop small section, and write some tests
+# to see if they work as expected. If you run a file as `python main.py`, any
+# method calls out in the open, like
+main()
+# will be called properly. Use this to do some quick testing, e.g. to make
+# sure that the DependencyTree works like it should
