0454cd1427bff84eb317aa773aa29d4ec59cfc4d
Marcus Gidekull <gidekull@Marcuss-MacBook-Pro.local>
2015-12-03 14:26:58 +0900

    Bug fixes + added dependency vocab.

--- vocabulary.py
+++ vocabulary.py
@@ -1,10 +1,14 @@
 import pickle
 
-def vocabulary(filen)
+def vocabulary(filen):
+	"""Takes a file as input, unpickles it and add every entity 
+	of it to the vocabulary. Returns vocabulary."""
+
 	with open (filen, 'rb') as f:
 		input =  pickle.load(f)
 
 	vocab = {}
+	dep_vocab = {}
 
 	for k in range(len(input)):
 		for l in range(len(input[k])):
@@ -14,3 +18,7 @@ def vocabulary(filen)
 				else:
 					print "added " + input[k][l][m][0]
 					vocab[input[k][l][m][0]] = len(vocab) + 1
+				if input[k][l][1][0] in dep_vocab:
+					dep_vocab[[k][l][1][0]] = len(dep_vocab) + 1
+
+	return vocab
