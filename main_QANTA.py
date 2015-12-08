2e99c56ebed3ac5e2349a825e2c0535a46267a82
Thorbjørn Wolf <thorbjorn.wolf@gmail.com>
2015-12-08 17:28:50 +0900

    Reverted set choice temporarily

--- main_QANTA.py
+++ main_QANTA.py
@@ -7,19 +7,19 @@ def run(input_folder, dimensionality):
 
     # dictionary with all info
     question_info_path = os.path.join(input_folder, "question_info")
-    question_info_path = question_info_path + "_" + set_choice
+    question_info_path = question_info_path #+ "_" + set_choice
 
     # dictionary with all the words
     vocabulary_path = os.path.join(input_folder, "vocabulary")
-    vocabulary_path = vocabulary_path + "_" + set_choice
+    vocabulary_path = vocabulary_path #+ "_" + set_choice
 
     # dictionary with all the dependencies
     dependency_path = os.path.join(input_folder, "dependency_vocabulary")
-    dependency_path = dependency_path + "_" + set_choice
+    dependency_path = dependency_path #+ "_" + set_choice
 
     # list of all the tree
     tree_list_path = os.path.join(input_folder, "tree_list")
-    tree_list_path = tree_list_path + "_" + set_choice
+    tree_list_path = tree_list_path #+ "_" + set_choice
 
     with open(vocabulary_path, 'rb') as vocabulary_file:
         vocabulary = cPickle.load(vocabulary_file)
