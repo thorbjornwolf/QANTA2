3c1c6ac80d93aa2769394f20686fb47facc5ed65
Thorbjørn Wolf <thorbjorn.wolf@gmail.com>
2015-12-08 17:26:55 +0900

    Converted tabs to spaces

--- main_QANTA.py
+++ main_QANTA.py
@@ -21,28 +21,28 @@ def run(input_folder, dimensionality):
     tree_list_path = os.path.join(input_folder, "tree_list")
     tree_list_path = tree_list_path + "_" + set_choice
 
-	with open(vocabulary_path, 'rb') as vocabulary_file:
-		vocabulary = cPickle.load(vocabulary_file)
+    with open(vocabulary_path, 'rb') as vocabulary_file:
+        vocabulary = cPickle.load(vocabulary_file)
 
-	with open(dependency_path, 'rb') as dependency_file:
-		dependency = cPickle.load(dependency_file)
+    with open(dependency_path, 'rb') as dependency_file:
+        dependency = cPickle.load(dependency_file)
 
-	with open(answers_path, 'rb') as answerfile:
-		answers = cPickle.load(answerfile)
+    with open(answers_path, 'rb') as answerfile:
+        answers = cPickle.load(answerfile)
 
-	with open(tree_list_path, 'rb') as tree_list_file:
-		tree_list = cPickle.load(tree_list_file)
+    with open(tree_list_path, 'rb') as tree_list_file:
+        tree_list = cPickle.load(tree_list_file)
 
-	qanta = QANTA(dimensionality, vocabulary, dependency)
+    qanta = QANTA(dimensionality, vocabulary, dependency)
 
-	qanta.train(tree_list, n_incorrect_answers=100, n_epochs=1,
-					print_training_accuracy=True)
+    qanta.train(tree_list, n_incorrect_answers=100, n_epochs=1,
+                    print_training_accuracy=True)
 
 
 if __name__ == '__main__':
-	source_path = 'output-hist'
-	dimensionality = 50
+    source_path = 'output-hist'
+    dimensionality = 50
 
-	print "Running main_QANTA for data in {}".format(source_path)
+    print "Running main_QANTA for data in {}".format(source_path)
 
-	run(source_path, dimensionality)
+    run(source_path, dimensionality)
