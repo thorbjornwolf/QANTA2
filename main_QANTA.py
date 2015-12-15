b5f7bac6d58d19afce918046493b4839002224d6
gidekull <marcus@gidekull.se>
2015-12-15 19:17:06 +0900

    Added word2vec as well.

--- main_QANTA.py
+++ main_QANTA.py
@@ -3,13 +3,10 @@ import os
 import cPickle
 
 def run(input_folder, dimensionality, epochs):
+	"""Opens up the relevant files and calls the model. Saves the model every
+	10 epochs for later evaluation."""
 
 	set_choice = "train"
-	model_path = os.path.join(input_folder, 'model_wiki')
-
-	# dictionary with all info
-	question_info_path = os.path.join(input_folder, "question_info")
-	question_info_path = question_info_path + "_" + set_choice
 
 	# dictionary with all the words
 	vocabulary_path = os.path.join(input_folder, "vocabulary")
@@ -29,28 +26,26 @@ def run(input_folder, dimensionality, epochs):
 	with open(dependency_path, 'rb') as dependency_file:
 		dependency = cPickle.load(dependency_file)
 
-	# with open(answers_path, 'rb') as answerfile:
-	#     answers = cPickle.load(answerfile)
-
 	with open(tree_list_path, 'rb') as tree_list_file:
 		tree_list = cPickle.load(tree_list_file)
 
-	qanta = QANTA(dimensionality=dimensionality, vocabulary=vocabulary, dependency_dict=dependency, embeddings_file="text8.model")
+	qanta = QANTA(dimensionality=dimensionality, vocabulary=vocabulary,
+	 dependency_dict=dependency, embeddings_file="text8.model")
 
 	for k in range(epochs/10):
 		epoch_path = str((k+1)*10)
 		qanta.train(tree_list, n_incorrect_answers=100, n_epochs=10) 
 		temp_path = os.path.join(input_folder, epoch_path)
-		with open(model_path, 'wb') as f:
+		with open(temp_path, 'wb') as f:
 			cPickle.dump(qanta, f)
 		print "Saved model in {}".format(temp_path)
 
 
 
 if __name__ == '__main__':
-	source_path = 'history'
-	dimensionality = 75
-	epochs = 80
+	source_path = 'wiki' #directory of the preprocessed data
+	dimensionality = 75 #dimensionality of wordvectors
+	epochs = 200 #number of epochs to train over
 
 	print "Running main_QANTA for data in {}".format(source_path)
 
